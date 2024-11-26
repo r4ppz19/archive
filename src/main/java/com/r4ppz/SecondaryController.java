69894533640e74c054b2a0be1e8c4193c7710e19
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-11-26 19:42:36 +0800

    Initialize repo for creating archive software by r4ppz

---
+++ src/main/java/com/r4ppz/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.r4ppz;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.r4ppz {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+
+    opens com.r4ppz to javafx.fxml;
+    exports com.r4ppz;
+}
