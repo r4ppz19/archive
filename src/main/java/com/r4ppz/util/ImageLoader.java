1f880435acb0f59771dc8618d7c3248b14321b65
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-09 10:03:07 +0800

    Implement image caching in ImageLoader to optimize image loading performance

--- src/main/java/com/r4ppz/util/ImageLoader.java
+++ src/main/java/com/r4ppz/util/ImageLoader.java
@@ -1,13 +1,28 @@
 package com.r4ppz.util;
 
 import java.util.Objects;
+import java.util.Map;
+import java.util.HashMap;
 
 import javafx.scene.image.Image;
 
 public class ImageLoader {
 
+    // Cache to store loaded iamges
+    private Map<String, Image> imageCache = new HashMap<>();
+
     public Image loadImage(String imagePath) {
+
+        // Check if the image is already in the cache
+        if (imageCache.containsKey(imagePath)) {
+            return imageCache.get(imagePath);
+        }
+
+        // Load the image from the disk
         Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(imagePath)));
+        // Stores the loaded image in the cache
+        imageCache.put(imagePath, image);
+
         return image;
     }
 }
