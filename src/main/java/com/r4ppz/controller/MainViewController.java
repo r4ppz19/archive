4666cf85fdfa635f9bdc02a7b34bffc198747cd5
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-09 19:27:57 +0800

    Implement dynamic button movement on hover in MainViewController; update FXML for button layout and label text

--- src/main/java/com/r4ppz/controller/MainViewController.java
+++ src/main/java/com/r4ppz/controller/MainViewController.java
@@ -1,14 +1,34 @@
 package com.r4ppz.controller;
 
+import java.util.Random;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class MainViewController {
+    private int width = 400;
+    private int hight = 200;
+
+    Random random = new Random();
+
+    @FXML
+    public void initialize() {
+        fuckYouButton.setOnMouseEntered(event -> moveButton());
+    }
+
     @FXML
     private Button fuckYouButton;
     
     @FXML
-    public void fuckYouButtonAction() {
+    public void fuckYouButtonAction(ActionEvent actionEvent) {
         
+    }
+
+    public void moveButton() {
+        int x = random.nextInt(width + 1);
+        int y = random.nextInt(hight + 1);
+        fuckYouButton.setLayoutX(x);
+        fuckYouButton.setLayoutY(y);
     }
 }
