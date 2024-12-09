395af6d04ad2cc9ee24cc321ec8b5f113a7ca1c3
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-09 13:36:55 +0800

    Update README to remove build and run instructions; add DefaultUser model class

---
+++ src/main/java/com/r4ppz/model/DefaultUser.java
@@ -0,0 +1,14 @@
+package com.r4ppz.model;
+
+public class DefaultUser {
+    private String username = "r4ppz";
+    private String password = "12312005";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
