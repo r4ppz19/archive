142a868888a54da4ce58f16bb1e07372fd58d552
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-09 19:06:38 +0800

    Add MainView components and styling; implement button action in MainViewController

--- src/main/java/com/r4ppz/controller/MainViewController.java
+++ src/main/java/com/r4ppz/controller/MainViewController.java
@@ -1,5 +1,14 @@
 package com.r4ppz.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainViewController {
+    @FXML
+    private Button fuckYouButton;
     
+    @FXML
+    public void fuckYouButtonAction() {
+        
+    }
 }
